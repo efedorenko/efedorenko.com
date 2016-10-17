0e9623cd42385f0cd8b183bf526eb594784d2f1d
Eugene Fedorenko <fedorenko@gmail.com>
2016-10-16 22:06:22 -0400

    Updated config

--- craft/config/general.php
+++ craft/config/general.php
@@ -8,27 +8,36 @@
  */
 
 return array(
-	'*' => array(
-		'omitScriptNameInUrls' => true,
-		'appId' => 'efedorenko',
-		'defaultImageQuality' => 85
-	),
+  '*' => array(
+    'omitScriptNameInUrls' => true,
+    'appId' => 'efedorenko',
+    'defaultImageQuality' => 85
+  ),
 
-	/* Local environment */
-	'efedorenko.craft' => array(
-		'devMode' => true,
-		'siteUrl' => 'http://efedorenko.craft:8888/',
-		'environmentVariables' => array(
-			'baseUrl'  => 'http://efedorenko.craft:8888/',
-		),
-		'testToEmailAddress' => 'fedorenko@gmail.com',
-	),
+  /* Local environment */
+  'efedorenko.craft' => array(
+    'devMode' => true,
+    'siteUrl' => 'http://efedorenko.craft:8888/',
+    'environmentVariables' => array(
+      'baseUrl'  => 'http://efedorenko.craft:8888/',
+    ),
+    'testToEmailAddress' => 'fedorenko@gmail.com',
+  ),
 
-	/* Live environment */
-	'efedorenko.com' => array(
-		'siteUrl' => 'http://efedorenko.com/',
-		'environmentVariables' => array(
-			'baseUrl'  => 'http://efedorenko.com/',
-		)
-	)
+  /* Live environment */
+
+  '107.170.36.109' => array(
+    'siteUrl' => 'http://107.170.36.109/',
+    'environmentVariables' => array(
+      'basePath' => '/var/www/html/',
+      'baseUrl'  => 'http://107.170.36.109/',
+    )
+  ),
+
+  'efedorenko.com' => array(
+    'siteUrl' => 'http://efedorenko.com/',
+    'environmentVariables' => array(
+      'baseUrl'  => 'http://efedorenko.com/',
+    )
+  )
 );
